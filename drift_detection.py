c5956ac80166fe8ad64e6eabcae2dde8d0191223
Ubuntu <ubuntu@ip-172-31-41-211.us-west-2.compute.internal>
2020-01-03 02:17:01 +0000

    adicionando main jupyter

--- drift_detection.py
+++ drift_detection.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
-
+import scipy.stats
 
 def get_metrics(drifts, resp, window_size=100, verbose=False):
     """
